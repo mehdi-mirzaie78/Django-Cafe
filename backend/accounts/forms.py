c1662c9c17182d1aa5c2eb6d737e59ff1f527ff8
mahdi <mehdimirzaie1378@gmail.com>
2024-01-22 09:56:03 +0330

    accounts | forms.py | added essential forms for user

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.models import AUTH_USER_MODEL as User
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.utils.translation import gettext_lazy as _
@@ -10,8 +10,8 @@ class UserCreationForm(forms.ModelForm):
     password2 = forms.CharField(label=_("Confirm Password"), widget=forms.PasswordInput)
 
     class Meta:
-        model = User
-        fields = ("email", "phone", "first_name", "last_name")
+        model = get_user_model()
+        fields = ("email", "phone", "first_name", "last_name", "gender", "image")
 
     def clean_password2(self):
         cd = self.cleaned_data
@@ -33,13 +33,24 @@ class UserChangeForm(forms.ModelForm):
     )
 
     class Meta:
-        model = User
-        fields = ["email", "phone", "first_name", "last_name", "password", "last_login"]
+        model = get_user_model()
+        fields = [
+            "email",
+            "phone",
+            "first_name",
+            "last_name",
+            "gender",
+            "password",
+            "image",
+            "last_login",
+        ]
         labels = {
             "email": _("email"),
             "phone": _("phone number"),
             "first_name": _("first_name"),
             "last_name": _("last_name"),
+            "gender": _("gender"),
             "password": _("password"),
+            "image": _("image"),
             "last_login": _("last_login"),
         }
