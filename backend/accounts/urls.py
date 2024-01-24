a651b85d8610a0060d6dfb04cbd80656494e5b43
mahdi <mehdimirzaie1378@gmail.com>
2024-01-24 21:50:08 +0330

    accounts | urls.py | added profile path

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("auth/register/", views.RegisterView.as_view(), name="register"),
     path("auth/login/", views.LoginView.as_view(), name="login"),
     path("auth/refresh-token/", views.RefreshTokenView.as_view(), name="refresh"),
+    path("profile/", views.ProfileView.as_view(), name="profile")
     # path("auth-logout/", views.LogoutView.as_view(), name="logout"),
 ]
