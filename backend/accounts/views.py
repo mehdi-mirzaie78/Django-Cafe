a3ceff6dd5ff5b2cbd1b9d8943ce5bd9bd3a030e
MARK <mehdimirzaie1378@gmail.com>
2024-01-24 14:18:57 +0330

    accounts | views.py | added basic views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import UserRegisterSerializer
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serialized_data = UserRegisterSerializer(request.data)
+        serialized_data.is_valid(raise_exception=True)
+        serialized_data.save()
+        return Response(serialized_data, status=status.HTTP_201_CREATED)
+    
+
+class LoginView(APIView):
+    def post(self, request):
+        pass
+
+
+class RefreshView(APIView):
+    pass
+
+
+class LogoutView(APIView):
+    pass
