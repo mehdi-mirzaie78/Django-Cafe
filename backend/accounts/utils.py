e842558bbbaeaddc7fe67f6e62d4a71005ac89ea
mahdi <mehdimirzaie1378@gmail.com>
2024-01-25 14:55:32 +0330

    accounts | utils.py | added otp handler

--- backend/accounts/utils.py
+++ backend/accounts/utils.py
@@ -2,6 +2,7 @@ import jwt
 from uuid import uuid4
 from datetime import datetime, timedelta
 from django.conf import settings
+from django.core.cache import cache
 
 
 class JWTHandler:
@@ -56,3 +57,65 @@ class JWTHandler:
         refresh_token = cls.generate_refresh_token(user)
         data = {"access_token": access_token, "refresh_token": refresh_token}
         return data
+
+
+class OTPHandler:
+    MAX_DIGIT = settings.OTP_MAX_DIGIT
+    TIMEOUT = settings.OTP_TIMEOUT
+    VERIFICATION_KEY = "is_verified"
+    OTP_KEY = "otp"
+
+    @classmethod
+    def get_otp_key(cls, phone):
+        return f"{phone}-{cls.OTP_KEY}"
+
+    @classmethod
+    def get_verification_key(cls, phone):
+        return f"{phone}-is_verified"
+
+    @classmethod
+    def generate_random_otp(cls):
+        return str(uuid4().node)[: cls.MAX_DIGIT]
+
+    @classmethod
+    def save(cls, key, value, timeout=None):
+        if timeout is None:
+            cache.set(key, value)
+        else:
+            cache.set(key, value, timeout)
+
+    @classmethod
+    def save_otp(cls, phone, otp):
+        key = cls.get_otp_key(phone)
+        cls.save(key, otp, cls.TIMEOUT)
+
+    @classmethod
+    def send_otp(cls, phone):
+        # some steps
+        otp = cls.generate_random_otp()
+        cls.save_otp(phone, otp)
+        print(f"{phone} - {otp}")
+        return otp
+
+    @classmethod
+    def is_verified(cls, phone):
+        key = cls.get_verification_key(phone)
+        return cache.get(key)
+
+    @classmethod
+    def check_otp(cls, phone, otp):
+        key = cls.get_otp_key(phone)
+        stored_otp = cache.get(key)
+        return stored_otp == otp
+
+    @classmethod
+    def save_verified_phone(cls, phone):
+        key = cls.get_verification_key(phone)
+        cache.set(key, True)
+
+    @classmethod
+    def clean_cache(cls, phone):
+        otp_key = cls.get_otp_key(phone)
+        verification_key = cls.get_verification_key(phone)
+        cache.delete(otp_key)
+        cache.delete(verification_key)
