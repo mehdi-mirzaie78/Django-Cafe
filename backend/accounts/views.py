44a4469e010293d1a259e6fc6b793a9a765e1e7e
mahdi <mehdimirzaie1378@gmail.com>
2024-01-26 21:29:32 +0330

    accounts | views.py | modified refresh token view & added logout view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,7 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework import status
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
-from rest_framework.parsers import MultiPartParser, JSONParser
+from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from django.utils.translation import gettext_lazy as _
@@ -82,15 +83,31 @@ class RefreshTokenView(APIView):
         serialized_data.is_valid(raise_exception=True)
         refresh_token = serialized_data.validated_data["refresh_token"]
         user = JWTAuthentication().get_user_by_refresh_token(refresh_token)
-        data = JWT.generate_access_refresh_token(user)
+        access_token = JWT.generate_access_token(user)
         return Response(
-            {"message": _("Access token refreshed successfully"), **data},
+            {
+                "message": _("Access token refreshed successfully"),
+                "access_token": access_token,
+            },
             status=status.HTTP_200_OK,
         )
 
 
 class LogoutView(APIView):
-    pass
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        try:
+            serialized_data = RefreshTokenSerializer(data=request.data)
+            serialized_data.is_valid(raise_exception=True)
+            refresh_token = serialized_data.validated_data["refresh_token"]
+            JWTAuthentication().logout(refresh_token)
+            return Response(
+                {"message": _("Logged out successfully")}, status=status.HTTP_200_OK
+            )
+        except ValidationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ProfileView(APIView):
