a9c99f39ac60e5bbf84f09d469ad54dc828d6b4f
mahdi <mehdimirzaie1378@gmail.com>
2024-01-28 12:12:16 +0330

    core | admin.py | added base tabular inline

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -42,3 +42,37 @@ class BaseAdmin(admin.ModelAdmin):
     ]
 
     actions = ["logical_delete", "logical_restore", "deactivate", "activate"]
+
+
+class BaseTabularInline(admin.TabularInline):
+    readonly_fields = [
+        "created_at",
+        "deleted_at",
+        "restored_at",
+        "updated_at",
+        "is_deleted",
+        "is_active",
+        "created_by",
+        "updated_by",
+    ]
+    # def save_formset(self, request, obj, formset, change):
+    #     instances = formset.save(commit=False)
+    #     for instance in instances:
+    #         if not instance.pk:
+    #             instance.created_by = request.user
+    #         if instance.changed_data:
+    #             instance.updated_by = request.user
+    #         instance.save()
+    #     formset.save()
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            obj.created_by = request.user
+        if change:
+            obj.updated_by = request.user
+        super().save_model(request, obj, form, change)
+
+    def get_extra(self, request, obj=None, **kwargs):
+        if obj:
+            return 0
+        return 1
