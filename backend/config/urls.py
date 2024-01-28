c57dbeb485bf71b3d5fedf477321d4c4e83e7407
mahdi <mehdimirzaie1378@gmail.com>
2024-01-28 12:11:41 +0330

    config | urls.py | modified urls with prefix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -5,12 +5,12 @@ from django.utils.translation import gettext_lazy as _
 
 admin.site.site_header = _(settings.SITE_HEADER)
 admin.site.index_title = _(settings.INDEX_TITLE)
-
+prefix = "api/v1"
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls", namespace="home")),
-    path("accounts/", include("accounts.urls", namespace="accounts")),
-    path("actions/", include("actions.urls", namespace="actions")),
-    path("products/", include("products.urls", namespace="products")),
-    path("orders/", include("orders.urls", namespace="orders")),
+    path(f"{prefix}/", include("home.urls", namespace="home")),
+    path(f"{prefix}/accounts/", include("accounts.urls", namespace="accounts")),
+    path(f"{prefix}/actions/", include("actions.urls", namespace="actions")),
+    path(f"{prefix}/products/", include("products.urls", namespace="products")),
+    path(f"{prefix}/orders/", include("orders.urls", namespace="orders")),
 ]
