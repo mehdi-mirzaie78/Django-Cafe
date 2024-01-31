3b3694f098c183a56b4e16fcc090d5ab4d3cc1f8
MARK <mehdimirzaie1378@gmail.com>
2024-01-31 14:59:21 +0330

    frontend | finished base products

--- frontend/pages/products.jsx
+++ frontend/pages/products.jsx
@@ -1,32 +1,97 @@
-import Product from "../src/components/Product.js";
+import Product from "@/src/components/Product.js";
+import Layouts from "@/src/layouts/Layouts.js";
 import { useEffect, useState } from "react";
 import React from "react";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [menu, setMenu] = useState({});
   async function getProducts() {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/api/v1/menu/`,
       { method: "GET", headers: { "Content-Type": "application/json" } }
     );
-    const data = await response.json();
 
+    const data = await response.json();
+    console.log(data);
+    setMenu(data);
     setProducts(data.products);
   }
   useEffect(() => {
     getProducts();
   }, []);
+
   return (
-    <div>
-      <h1>Products</h1>
-      <div className="row">
-        {products &&
-          products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        {console.log(products)}
-      </div>
-    </div>
+    <Layouts>
+      {/* Section Started Inner */}
+      <section className="section kf-started-inner">
+        <div
+          className="kf-parallax-bg js-parallax"
+          style={{
+            backgroundImage: `url(${menu.image})`,
+          }}
+        />
+        <div className="container">
+          <h1
+            className="kf-h-title text-anim-1 scroll-animate"
+            data-splitting="chars"
+            data-animate="active"
+          >
+            {menu && menu.title}
+          </h1>
+        </div>
+      </section>
+      <section className="section kf-menu kf-menu-tabs">
+        <div className="container">
+          <div
+            className="kf-filter kf-filter-menu element-anim-1 scroll-animate"
+            data-animate="active"
+          >
+            <a
+              className="c-pointer active"
+              // onClick={handleFilterKeyChange("*")}
+              data-href="*"
+            >
+              All
+            </a>
+            <a
+              className="c-pointer"
+              // onClick={handleFilterKeyChange("fast-food")}
+              data-href="fast-food"
+            >
+              Fast food
+            </a>
+            <a
+              className="c-pointer"
+              // onClick={handleFilterKeyChange("hot-coffee")}
+              data-href="hot-coffee"
+            >
+              Hot coffee
+            </a>
+            <a
+              className="c-pointer"
+              // onClick={handleFilterKeyChange("dessert")}
+              data-href="dessert"
+            >
+              Dessert
+            </a>
+          </div>
+          <div
+            className="kf-menu-items"
+            style={{ backgroundImage: "url(images/menu_logo.png)" }}
+          >
+            <div className="row all-menu-items">
+              {products &&
+                products.map((product) => (
+                  <Product key={product.id} product={product} />
+                ))}
+              {!products && <div>There are no products</div>}
+              {console.log(products)}
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layouts>
   );
 };
 
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -2,10 +2,30 @@ import React from "react";
 
 const Product = ({ product }) => {
   return (
-    <div className="col-sm-6 col-md-3 border d-flex justify-content-center">
-      <img src={product.media[0].file} alt={product.name} />
-      <h1>{product.name}</h1>
-      <p>{product.price}</p>
+    <div className="kf-menu-item-col col-xs-12 col-sm-12 col-md-12 col-lg-6 all hot-coffee">
+      <div
+        className="kf-menu-item element-anim-1 scroll-animate"
+        data-animate="active"
+      >
+        <div className="image kf-image-hover">
+          <a href={product.media[0].file} className="has-popup-image">
+            <img
+              className="rounded"
+              src={product.media[0].file}
+              alt={product.name}
+              width="200px"
+              height="200px"
+              style={{ objectFit: "cover" }}
+            />
+          </a>
+        </div>
+
+        <div className="desc">
+          <h5 className="name">{product.name}</h5>
+          <div className="subname">{product.description}</div>
+          <div className="price">${product.price}</div>
+        </div>
+      </div>
     </div>
   );
 };
