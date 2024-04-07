90104fe0d032f85f58c1fff13ad2aa559d8491c8
Specter <mehdimirzaie1378@gmail.com>
2024-04-07 18:38:46 +0330

    frontend | Improving ui of products

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,6 +1,15 @@
-import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+import {
+  Badge,
+  Button,
+  Card,
+  CardBody,
+  HStack,
+  Heading,
+  Image,
+  Text,
+} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-
 import Product from "../entities/Product";
 
 interface Props {
@@ -9,13 +18,31 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <Card>
-      <Image src={product.media[0].file} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}></HStack>
-        <Heading fontSize="2xl">
-          <Link to={"/games/" + product.slug}>{product.name}</Link>
-        </Heading>
+    <Card p={2}>
+      <Image
+        src={product.media[0].file}
+        height={"sm"}
+        objectFit={"cover"}
+        borderRadius={10}
+      />
+      <CardBody>  
+        <HStack justifyContent="space-between" marginBottom={3}>
+          <Heading fontSize="2xl">
+            <Link to={"/products/" + product.slug}>{product.name}</Link>
+          </Heading>
+          <Heading fontSize="2xl">
+            <Badge colorScheme="green" fontSize={"2xl"} px={3} rounded={5}>
+              $ {product.price}
+            </Badge>
+          </Heading>
+        </HStack>
+
+        <Text>{product.description}</Text>
+        <HStack marginTop={5} justifyContent="center">
+          <Button>
+            <AddIcon color="blue.300" />
+          </Button>
+        </HStack>
       </CardBody>
     </Card>
   );
--- frontend/src/data/products.ts
+++ frontend/src/data/products.ts
@@ -1,43 +1,87 @@
 export default {
-  count: 2,
-  next: null,
-  previous: null,
-  results: [
-    {
-      id: 1,
-      name: "Test Product",
-      slug: "test-product",
-      description: "",
-      unit_price: 100,
-      discount: 0,
-      price: 100,
-      media: [
-        {
-          id: 1,
-          file_type: "image",
-          file: "https://s3.ir-thr-at1.arvanstorage.ir/cuppa-cloud/profile_AxFRVNf.jpg?AWSAccessKeyId=9d41d077-ee4e-4a73-83d2-bf1150572188&Signature=JgIzvU2GufIZNIvVyVhyvlD%2B99o%3D&Expires=1712496657",
-        },
-      ],
-      categories: [1],
-      stock: 10,
-    },
-    {
-      id: 2,
-      name: "Pizza Special",
-      slug: "pizza-special",
-      description: "",
-      unit_price: 200,
-      discount: 0,
-      price: 200,
-      media: [
-        {
-          id: 2,
-          file_type: "image",
-          file: "https://s3.ir-thr-at1.arvanstorage.ir/cuppa-cloud/pizza.jpeg?AWSAccessKeyId=9d41d077-ee4e-4a73-83d2-bf1150572188&Signature=IlwHVL5IxURpSeluQxyrFk83JLo%3D&Expires=1712496657",
-        },
-      ],
-      categories: [2],
-      stock: 20,
-    },
-  ],
-};
+  "count": 4,
+  "next": null,
+  "previous": null,
+  "results": [
+      {
+          "id": 1,
+          "name": "Espresso",
+          "slug": "espresso",
+          "description": "A concentrated coffee beverage brewed by forcing a small amount of nearly boiling water through finely-ground coffee beans.",
+          "unit_price": 3,
+          "discount": 0,
+          "price": 3,
+          "media": [
+              {
+                  "id": 1,
+                  "file_type": "image",
+                  "file": "https://s3.ir-thr-at1.arvanstorage.ir/cuppa-cloud/espresso.png?AWSAccessKeyId=9d41d077-ee4e-4a73-83d2-bf1150572188&Signature=xBuraJz3izT9h24yZeUr3zgCC6Q%3D&Expires=1712504047"
+              }
+          ],
+          "categories": [
+              1
+          ],
+          "stock": 10
+      },
+      {
+          "id": 2,
+          "name": "Americano",
+          "slug": "americano",
+          "description": "Made by diluting espresso with hot water, giving it a similar strength to traditional brewed coffee but with a different flavor profile.",
+          "unit_price": 4,
+          "discount": 0,
+          "price": 4,
+          "media": [
+              {
+                  "id": 2,
+                  "file_type": "image",
+                  "file": "https://s3.ir-thr-at1.arvanstorage.ir/cuppa-cloud/americano.jpeg?AWSAccessKeyId=9d41d077-ee4e-4a73-83d2-bf1150572188&Signature=36Yb0Z7U1qkk4d9RXfQIG8fpKaI%3D&Expires=1712504047"
+              }
+          ],
+          "categories": [
+              1
+          ],
+          "stock": 10
+      },
+      {
+          "id": 3,
+          "name": "Latte",
+          "slug": "latte",
+          "description": "A coffee drink made with espresso and steamed milk, often topped with a small amount of milk foam.",
+          "unit_price": 5,
+          "discount": 0,
+          "price": 5,
+          "media": [
+              {
+                  "id": 3,
+                  "file_type": "image",
+                  "file": "https://s3.ir-thr-at1.arvanstorage.ir/cuppa-cloud/latte.jpg?AWSAccessKeyId=9d41d077-ee4e-4a73-83d2-bf1150572188&Signature=1vRn%2Bv8yAH%2FPBxLUnFoDPvwmRfk%3D&Expires=1712504047"
+              }
+          ],
+          "categories": [
+              1
+          ],
+          "stock": 10
+      },
+      {
+          "id": 4,
+          "name": "Cappuccino",
+          "slug": "cappuccino",
+          "description": "A coffee drink consisting of espresso, steamed milk, and a layer of frothed milk foam on top.",
+          "unit_price": 7,
+          "discount": 0,
+          "price": 7,
+          "media": [
+              {
+                  "id": 4,
+                  "file_type": "image",
+                  "file": "https://s3.ir-thr-at1.arvanstorage.ir/cuppa-cloud/cappuccino.jpg?AWSAccessKeyId=9d41d077-ee4e-4a73-83d2-bf1150572188&Signature=XP7gjAqAN71nzPkvEtMYxt7BKJ8%3D&Expires=1712504047"
+              }
+          ],
+          "categories": [
+              1
+          ],
+          "stock": 10
+      }
+  ]
+}
