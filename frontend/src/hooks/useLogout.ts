d14654bf7f51ef45d606ee9becaa129880dd5d8d
specter <mehdimirzaie1378@gmail.com>
2024-05-13 16:35:08 +0330

    frontend | fix | fixing logout hook for redirection

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -2,8 +2,11 @@ import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
 import PrivateAPIClient from "../services/privateAPIClient";
 import useAuthQueryStore from "../store/authStore";
+import { useNavigate } from "react-router-dom";
 
 const useLogout = () => {
+  const navigate = useNavigate();
+
   const { authQuery, setAuthQuery } = useAuthQueryStore();
 
   const privateAPIClient = new PrivateAPIClient("accounts/auth/logout/");
@@ -14,6 +17,7 @@ const useLogout = () => {
       privateAPIClient.post({ refreshToken: authQuery.refreshToken }),
     onSettled: () => {
       setAuthQuery({});
+      navigate("/login");
     },
   });
 };
